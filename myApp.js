3265e1bc609507110ad0849ab2018dfd054e4037
Kashmika <kashmikat@gmail.com>
2025-02-23 18:10:21 +0530

    test

--- myApp.js
+++ myApp.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3030;
 
-app.listen(PORT, () => {
-  console.log(`FreecodeCamp sec app started on port ${PORT}`);
-});
 
 
 
